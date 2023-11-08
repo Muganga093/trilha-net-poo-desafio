016e6c997739860b4cedae5b6d9b2b6cb9f3b3a9
Kel Lopes <144739819+Muganga093@users.noreply.github.com>
2023-11-08 13:00:08 -0300

    Update Nokia.cs

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,17 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    // IMPLEMENTADO
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // IMPLEMENTADO
+        public Nokia (string numero, string modelo, string imei, int memoria) : base (numero, modelo, imei, memoria)
+        {
+
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine ($"{nomeApp} instalado no seu Nokia");
+        }
     }
 }
